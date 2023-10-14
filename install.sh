cf78c1e5792c3ad4220b00425f8ce30a2dae1be9
Avenfenix <thedonjuan008p@gmail.com>
2023-10-14 10:40:33 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo apt-get install -y libegl1-mesa-dev libgbm-dev libgles2-mesa-dev
+sudo apt install -y g++ libglu1-mesa-dev libasound2-dev libfreetype6-dev libxpm-dev libxft-dev libxxf86vm-dev libpulse-dev libopenal-dev libwebkitgtk-dev libaudio-dev libesd0-dev libwebkit2gtk-4.0-dev libgtk-3-dev
